f0c7ccaee27e760a4a96b3eb49a77a1743595e79
FelixKengerli <ifelru@mail.ru>
2023-10-12 03:08:08 +0300

    *Added "ResolutionManager" *Updated "LevelSample"

---
+++ Assets/CodeBase/ResolutionManager.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class ResolutionManager : MonoBehaviour
+{
+    [SerializeField] private int _targetScreenWidth = 720;
+    [SerializeField] private int _targetScreenHeight = 1280;
+
+    private void Start() =>
+        SetResolution(_targetScreenWidth, _targetScreenHeight);
+
+    private void SetResolution(int width, int height) =>
+        Screen.SetResolution(width, height, false);
+}
